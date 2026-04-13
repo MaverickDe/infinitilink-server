a00295f9684f8e609f18989a9b70fa95a3980fda
prince <princewillasotibe123@gmail.com>
2026-04-13 06:41:38 +0100

    rm db mgr

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,13 +1,9 @@
-import { BankTransactionService } from "../services/bankAdmin/transactions.service";
-
 
 
 
 export class JobService{
 
-static async  userTransactionIngestion (job){
-  return await BankTransactionService.queueIngestTransactions(job)
-}
+
 
 
 }
